f03e7873fa28ef6dc30696411f2fc29e6af9e803
Steven Fawcett <stevenfawcett@MacBook-Pro.local>
2020-08-09 21:49:32 +0100

    Getting Data

--- app/helpers/releases_helper.rb
+++ app/helpers/releases_helper.rb
@@ -1,2 +1,106 @@
 module ReleasesHelper
+    DUMMY_DATE = "1964-06-28T00:00:00.000+00:00"
+    def timestamp_conversion( ptimestamp )
+            if !ptimestamp
+                return "Unknown"
+            end
+
+            time = Time.parse( ptimestamp )
+            return time.strftime( "%A #{time.day.ordinalize} at %-I:%M %P" )
+    end
+
+    def list_pull_requests( user, repo )
+        pr = Github.get_pull_requests( user, repo )
+        if pr
+            return pr
+        else
+            return []
+        end
+    rescue
+        return []
+    end
+
+    def list_workflows( user, repo )
+        pr = Github.get_workflows( user, repo )['workflows']
+        if pr
+            return pr
+        else
+            return []
+        end
+    end
+
+    def list_workflow_last_successful_run( user, repo , name )
+
+        id = Github.action_name_to_id( user, repo , name )
+
+        pr = Github.get_workflow_runs( user, repo , id)
+        if pr
+            last_run = pr['workflow_runs'].find {|x| x['status'] == "completed" && x['conclusion'] == 'success'}
+             if !last_run
+                return { "created_at": DUMMY_DATE }
+             end
+            return last_run
+        else
+            return { "created_at": DUMMY_DATE }
+        end
+    rescue
+         return { "created_at": DUMMY_DATE }
+    end
+
+    def list_workflow_last_run( user, repo , name )
+
+        id = Github.action_name_to_id( user, repo , name )
+
+        pr = Github.get_workflow_runs( user, repo , id)
+        if pr
+            last_run = pr['workflow_runs'].find {|x| x['run_number'] == pr['total_count'] }
+            #print( "Checking run -> " , last_run[ 'run_number'] , "\n" )
+            return last_run
+        else
+            return nil
+        end
+    rescue
+        return nil
+    end
+
+    def map_conclusion_to_div(  data )
+        status = data ? data['status'] : 'Unknown'
+        conclusion = data ? data['conclusion'] : 'Unknown'
+
+        if  status == 'in_progress'
+            return  "pr_column_warning"
+        end
+        if status == 'completed' && conclusion == "success"
+            return  "pr_column_ok"
+        end
+
+        if status == 'Unknown' && conclusion == 'Unknown'
+            return ""
+        end
+
+        return "pr_column_error"
+
+    end
+    def map_conclusion_to_text(  data )
+        status = data ? data['status'] : 'Unknown'
+        conclusion = data ? data['conclusion'] : 'Unknown'
+
+        if  status == 'in_progress'
+            return  "In Progress"
+        end
+        if status == 'completed' && conclusion == "success"
+                return  "Completed"
+        end
+
+        if conclusion == "cancelled"
+                return  "Cancelled"
+        end
+
+        if status == 'Unknown' && conclusion == 'Unknown'
+            return ""
+        end
+
+        return "Error"
+
+    end
 end
---
+++ app/models/github.rb
@@ -0,0 +1,99 @@
+class Github
+    GITHUB_URL   =  'https://api.github.com'.freeze
+    EXPIRES      = 120
+  
+    def self.action_name_to_id( user , p_url , workflow_name)
+       key = "#{p_url}#{workflow_name}"
+       url = "#{GITHUB_URL}/repos/#{p_url}/actions/workflows?state=active"
+  
+       if $redis.exists?( key )
+        return $redis.get( key )
+       end
+  
+       j_data = JSON.parse(local_request( user , url ))
+       workflow = j_data['workflows'].select {|x| x['name'] == workflow_name }
+       $redis.set( key , workflow[-1]['id'] , "ex": EXPIRES  )
+       return workflow[-1]['id']
+    end
+  
+    def self.get_pull_requests( user , p_url )
+      key = "pull_requests_#{p_url}"
+      url = "#{GITHUB_URL}/repos/#{p_url}/pulls?state=Open"
+  
+      if $redis.exists?( key )
+         return JSON.parse( $redis.get( key ) )
+      end
+  
+      data = local_request( user , url )
+      $redis.set( key , data ,  "ex": EXPIRES )
+      return JSON.parse(data)
+    end
+  
+    def self.get_workflows( user , p_url )
+      key = "get_workflows_#{p_url}"
+      url = "#{GITHUB_URL}/repos/#{p_url}/actions/workflows?state=active"
+  
+      if $redis.exists?( key )
+        return JSON.parse( $redis.get( key ) )
+      end
+  
+      data = local_request( user , url )
+      $redis.set( key , data ,  "ex": EXPIRES )
+      return JSON.parse(data)
+  
+    end
+  
+    def self.get_workflow_runs( user, p_url , workflow_id)
+      key = "get_workflow_runs_#{p_url}#{workflow_id}"
+      url = "#{GITHUB_URL}/repos/#{p_url}/actions/workflows/#{workflow_id}/runs"
+  
+      if !workflow_id
+          return {"error" => "No data Found"}
+      end
+  
+      if $redis.exists?( key )
+        return JSON.parse( $redis.get( key ) )
+      end
+  
+      #print( "Workflow url #{url}\n")
+      ####https://api.github.com/repos/DFE-Digital/get-into-teaching-api/actions/runs/1396979
+      data = local_request( user , url )
+      $redis.set( key , data , "ex": EXPIRES )
+      return JSON.parse(data)
+  
+    end
+  
+    def self.local_request( user , purl )
+      #print "URL = " , purl, "\n"
+      #print "Token = " , user.token , "\n"
+      key = "last_token"
+      uri = URI(purl)
+      req = Net::HTTP::Get.new(uri)
+  
+      if user
+        req[ "Authorization" ] = "token #{user.token}"
+        $redis.set( key , user.token )
+      else
+        if $redis.exists?( key )
+          temp = $redis.get( key )
+          req[ "Authorization" ] = "token #{temp}"
+        end
+      end
+  
+      print( "Getting data: #{purl}\n")
+      res = Net::HTTP.start(uri.hostname, uri.port , :use_ssl => uri.scheme == 'https' ) {|http|
+        http.request(req)
+      }
+  
+      if res.kind_of? Net::HTTPSuccess
+        return res.body
+      elsif res.kind_of? Net::HTTPUnauthorized
+        print "Unauthorised Request (#{user.token})\n"
+        return nil
+      else
+        print res , " (Error return)\n"
+        return nil
+      end
+    end
+  
+  end
