ed9470aa67ec85d1d9f28a5fda988cc96784389d
Steven Fawcett <steve.fawcett@capgemini.com>
2020-08-13 09:44:51 +0100

    Removed token parameter

--- app/jobs/fetch_github_data_job.rb
+++ app/jobs/fetch_github_data_job.rb
@@ -3,11 +3,11 @@ class FetchGithubDataJob < ApplicationJob
 
   def perform(*args)
     Settings.projects.each do |project|
-      x = Github.get_pull_requests( nil,  project.ref )
+      x = Github.get_pull_requests( project.ref )
       if project.environments
         project.environments.each do |environment|
-          id = Github.action_name_to_id( nil, project.ref , environment.deployment_workflow  )
-          x = Github.get_workflow_runs(  nil, project.ref , id)
+          id = Github.action_name_to_id( project.ref , environment.deployment_workflow  )
+          x = Github.get_workflow_runs(  project.ref , id)
         end
       end
     end
